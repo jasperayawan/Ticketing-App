d68453b756b93d54edec5643e34951e81812b9c9
jasperayawan <ejayawan22@gmail.com>
2023-12-28 00:53:15 +0800

    update navigation

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -5,14 +5,14 @@ import { IoIosHome } from "react-icons/io";
 import { FaTicket } from "react-icons/fa6";
 import Link from 'next/link'
 
-export default function Nav({ params }) {
+export default function Nav() {
   return (
     <nav className='flex justify-between items-center px-4 py-2 bg-nav'>
         <div className="flex flex-row gap-x-2 text-default-text">
-        <Link href='/'>
+        <Link href='/' passHref>
             <IoIosHome className='cursor-pointer'/>
         </Link>
-        <Link href='/TicketPage/123'>
+        <Link href='/TicketPage/123' passHref>
             <FaTicket className='cursor-pointer'/>
         </Link>
         </div>
